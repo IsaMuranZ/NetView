52957aee232164b1539b6928de8157d181c8a561
IsaMuran <imuran@ucsc.edu>
2024-08-03 13:18:50 +0200

    fix: database connection functional

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def create_app():
     app = Flask(__name__)
 
     # Database configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://netview_user:password@localhost/netview'
+    # this is hardcoded for now and should be changed later to a more graceful solution
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://netview_user:netviewadmin@localhost/netview'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
@@ -19,7 +21,6 @@ def create_app():
         from . import routes
 
         # Create database tables
-        #db.create_all()
+        db.create_all()
 
     return app
-
